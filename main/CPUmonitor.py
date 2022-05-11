dfd089aec17f600fd691a42d7237eb8e511247f4
friz25 <frizmob@gmail.com>
2022-05-11 17:49:52 +0300

    20 добвилм 4 прогрессбара

--- main/CPUmonitor.py
+++ main/CPUmonitor.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
+import sys
+from process import CpuBar
 
 class Application(tk.Tk):
     #создали окно интерфейс
@@ -11,14 +13,17 @@ class Application(tk.Tk):
         self.resizable(False, False)
         self.title('CPU-RAM usage monitor bar')
 
+        self.cpu = CpuBar()
         self.set_ui()
+        self.make_bar_cpu_usage()
+
     #Виджеты окна
     def set_ui(self):
         # создали кнопку, для удобства - через переменную
-        exit_but = ttk.Button(self, text='Exit', command=self.app_exit)
+        exit_but = ttk.Button(self, text='Закрыть прогу', command=self.app_exit)
         exit_but.pack(fill=tk.X) #один из 3х упаковщиков (еще есть grid(),по столбцам и строкам, и place(),по сетке)
 
-        self.bar2 = ttk.LabelFrame(self, text='Manual')
+        self.bar2 = ttk.LabelFrame(self, text='Управление') #(!)создали Фрейм (будем засовывать в него кнопки и т.д.)
         self.bar2.pack(fill=tk.X)
 
         self.combo_win = ttk.Combobox(self.bar2,
@@ -27,14 +32,29 @@ class Application(tk.Tk):
         self.combo_win.current(1)
         self.combo_win.pack(side=tk.LEFT)
 
-        ttk.Button(self.bar2, text='move').pack(side=tk.LEFT)
+        ttk.Button(self.bar2, text='двигать').pack(side=tk.LEFT)
         ttk.Button(self.bar2, text='>>>').pack(side=tk.LEFT)
 
-        self.bar2 = ttk.LabelFrame(self, text='Power')
-        self.bar2.pack(fill=tk.BOTH)
+        self.bar = ttk.LabelFrame(self, text='Нагрузка')
+        self.bar.pack(fill=tk.BOTH)
 
         self.bind_class('Tk', '<Enter>', self.enter_mouse)
         self.bind_class('Tk', '<Leave>', self.leave_mouse)
+
+    def make_bar_cpu_usage(self):
+        ttk.Label(self.bar, text=f'физ-е ядра: {self.cpu.cpu_count}, лог-е ядра: {self.cpu.cpu_count_logical}',
+                  anchor = tk.CENTER).pack(fill=tk.X)
+
+        self.list_label = []
+        self.list_pbar = []
+
+        for i in range(self.cpu.cpu_count_logical):
+            self.list_label.append(ttk.Label(self.bar, anchor=tk.CENTER))
+            self.list_pbar.append(ttk.Progressbar(self.bar, length=100))#Создаём 4 полоски (Прогрессбары)
+        for i in range(self.cpu.cpu_count_logical):
+            self.list_label[i].pack(fill=tk.X)
+            self.list_pbar[i].pack(fill=tk.X)
+
 
     def enter_mouse(self, event):
         if self.combo_win.current() == 0 or 1:
