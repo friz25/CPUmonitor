ab881b792fba0697b5ec3924521bf76f673b5664
friz25 <frizmob@gmail.com>
2022-05-11 18:04:49 +0300

    20 получаем данные CPU

---
+++ main/process.py
@@ -0,0 +1,20 @@
+# здесь работаем с модулем psutil (который позволит получать данные о CPU-RAM)
+import psutil as pt
+from time import sleep
+
+class CpuBar:
+
+    def __init__(self):
+        self.cpu_count = pt.cpu_count(logical=False)
+        self.cpu_count_logical = pt.cpu_count()
+        # print(self.cpu_count) #сколько ядер (у меня 2)
+        # print(self.cpu_count_logical) #сколько всего потоков (у меня 4)
+    def cpu_percent_return(self):
+        return pt.cpu_percent(percpu=True)
+
+if __name__ == '__main__':
+    x = CpuBar()
+
+    for i in range(10):
+        print(x.cpu_percent_return())
+        sleep(1)
